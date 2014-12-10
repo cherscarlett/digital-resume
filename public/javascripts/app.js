8c371e45369dea9e1c1a6f0fb33f0a45660a1799
Cher Stewart <cstewart@gannett.com>
2014-12-10 15:23:38 -0600

    new stuff

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,29 @@
 $("#sidebar nav li").live("click", function(){
-    var selectedTab = $(this).attr("id").replace("_tab", "");
-    var link = (selectedTab != 'character_summary') ? "/"+selectedTab : "/"; 
+    var selectedTab = $(this).attr("id").replace("_tab", ""),
+        link = (selectedTab != 'character_summary') ? "/"+selectedTab : "/"; 
     loadContent("main", selectedTab, link);
 });
+
+$(".controls a").live("click", function(){
+    var direction = $(this).attr("class");
+    moveGames(direction);
+});
+
+function moveGames(direction) {
+  $("."+direction).css("opacity", "0");
+  var games = $(".game-list-container"),
+      headings = $(".game-type-container");
+  if (direction == 'next') {
+    $(games).css("margin-left", "-300px");
+    $(headings).css("margin-left", "-300px");
+    $(".prev").css("opacity", "1");
+  }
+  else {
+    $(games).css("margin-left", "0");
+    $(headings).css("margin-left", "0");
+    $(".next").css("opacity", "1");
+  }
+}
 
 function loadContent(type, selectedTab, link) {
    $("#sidebar .current").removeClass("current");
